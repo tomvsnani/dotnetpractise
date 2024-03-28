676375ee1bedd82c1ad0d4f05c05130b3417d838
pramu326@gmail.com <pramu326@gmail.com>
2024-03-27 20:38:30 -0400

    commiting

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApplication1.Controllers
 {
     [ApiController]
-    
+
     public class WeatherForecastController : ControllerBase
     {
-       
+
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -14,43 +14,54 @@ namespace WebApplication1.Controllers
 
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger,IConfiguration configuration/*IHttpClientFactory clientFactory*/ )
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration/*IHttpClientFactory clientFactory*/ )
         {
 
 
             _logger = logger;
             _configuration = configuration;
-           /* _httpClientFactory = clientFactory;*/
+            /* _httpClientFactory = clientFactory;*/
 
         }
 
-/*
-        [HttpGet("/mc2")]
+        /*
+                [HttpGet("/mc2")]
 
-        public async Task<ActionResult<string>> sendtToMS2()
-        {
+                public async Task<ActionResult<string>> sendtToMS2()
+                {
 
-            var HttpClient = _httpClientFactory.CreateClient();
+                    var HttpClient = _httpClientFactory.CreateClient();
 
-            HttpClient.BaseAddress = new Uri(_configuration.GetSection("Urls")["MS2"]);
+                    HttpClient.BaseAddress = new Uri(_configuration.GetSection("Urls")["MS2"]);
 
-            var a= await HttpClient.GetAsync("/from1");
-
-            
+                    var a= await HttpClient.GetAsync("/from1");
 
 
 
-            return await a.Content.ReadAsStringAsync();
-        }*/
+
+
+                    return await a.Content.ReadAsStringAsync();
+                }*/
 
         [HttpGet("")]
 
-        public  string hello()
+        public string hello()
         {
 
             return "from ms1 revision";
         }
 
 
+
+
+        [HttpGet("/ok")]
+
+        public string helloo()
+        {
+
+            return "from ms2 revision";
+        }
+
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
 
+using Google.Api;
 using Microsoft.EntityFrameworkCore;
-
+using Serilog;
 using WebApplication1.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseSerilog((ctx,t) => {
+  
+    t.MinimumLevel.Information();
+
+    t.ReadFrom.Configuration(ctx.Configuration)
+   
+     
+     ;
+
+
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
+
+
+
+
 /*builder.Services.AddDbContext<SQLContext>(options => options.UseMySQL(connectionString: builder.Configuration.GetConnectionString("Mysql")).LogTo(Console.Write));
 */
 var app = builder.Build();
+
+
+
+
 
 
 
